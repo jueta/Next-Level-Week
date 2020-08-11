1c6fcf0d3f5ab6160871781bdde7562e54ef2bb5
joao pedro <jpmm.brasil@hotmail.com>
2020-08-11 02:35:01 -0300

    1 dia

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css'
+
+import PageHeader from '../../components/Page-Header';
+
+function TeacherForm() {
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrivel que voce quer dar aulas."/>
+    </div>
+  )
+}
+export default TeacherForm;
